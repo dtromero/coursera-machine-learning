4b92d3d579a33063946534509b99fcd7d6e12719
Daniel Romero <dtromero@gmail.com>
2015-08-25 16:47:59 -0400

    4/6 Complete with Octave Code

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -19,10 +19,11 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+h = sigmoid(X * theta);
 
-J = (1/m) * sum( (-y * log(sigmoid(X))) - ((1-y) * log(1 - sigmoid(X))));
+J = (1/m) * ((-y' * log(h)) - ((1-y') * log(1 - h)));
 
-grad = 
+grad = ((1/m) * ((h - y)' * X))';
 
 
 
